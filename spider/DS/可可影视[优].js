66a18b6f68cc796f25127d9aa008eda04514cea3
xiaoheiwawa <107086294+xiaoheiwawa@users.noreply.github.com>
2025-09-02 17:17:43 +0800

    Update 可可影视[优].js

--- spider/DS/可可影视[优].js
+++ spider/DS/可可影视[优].js
@@ -8,7 +8,7 @@ var rule = {
     quickSearch: 1,
     filterable: 0,
     headers: {
-         'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36 Edg/139.0.0.0'
+         'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36 Edg/139.0.0.0',
     },
 
     图片替换: function (input) {
