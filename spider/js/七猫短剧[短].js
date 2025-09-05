2b33d4db2a00ee729db4350da3c4496241c18a91
xiaoheiwawa <107086294+xiaoheiwawa@users.noreply.github.com>
2025-09-05 12:34:31 +0800

    Update 七猫短剧[短].js

--- spider/js/七猫短剧[短].js
+++ spider/js/七猫短剧[短].js
@@ -65,7 +65,7 @@ async function getHeaderX() {
 }
 
 var rule = {
-    title: '七猫短剧',
+    title: '七猫',
     host: h_ost,
     homeUrl: '/api/v1/playlet/index?tag_id=0&playlet_privacy=1&operation=1',
     searchUrl: '/api/v1/playlet/search?keyword=fykey&page=fypage',
