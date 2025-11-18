7efef05040012e9f9c2fb580c78fd9740bda66c6
xiaoheiwawa <107086294+xiaoheiwawa@users.noreply.github.com>
2025-11-18 12:38:53 +0800

    Update 斗球.js

--- spider/js/斗球.js
+++ spider/js/斗球.js
@@ -1,3 +1,13 @@
+/*
+@header({
+  searchable: 2,
+  filterable: 0,
+  quickSearch: 1,
+  title: '篮球录像吧',
+  '类型': '影视',
+  lang: 'ds'
+})
+*/
 var rule = {
     title:'篮球录像吧',
     host:'https://www.luxiangwu.com/',
